f9ab98fe2fa26755c4968f688adac52b00ff18a3
Paul Römer <paul@vaadin.com>
2019-01-25 20:27:16 +0100

    adds Vaadin Spring base starter

---
+++ src/main/java/org/vaadin/paul/spring/Application.java
@@ -0,0 +1,16 @@
+package org.vaadin.paul.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The entry point of the Spring Boot application.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
