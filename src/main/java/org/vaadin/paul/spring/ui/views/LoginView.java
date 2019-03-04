e5f6c9383de01e588aab81a0eaa205f5e34cbfd8
Paul Römer <paul@vaadin.com>
2019-03-04 07:38:47 +0100

    Java-only form based example

--- src/main/java/org/vaadin/paul/spring/ui/views/LoginView.java
+++ src/main/java/org/vaadin/paul/spring/ui/views/LoginView.java
@@ -1,25 +1,46 @@
 package org.vaadin.paul.spring.ui.views;
 
-import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.HtmlImport;
-import com.vaadin.flow.component.page.Viewport;
-import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
-import com.vaadin.flow.router.AfterNavigationEvent;
-import com.vaadin.flow.router.AfterNavigationObserver;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.PasswordField;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.dom.Element;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.server.InitialPageSettings;
-import com.vaadin.flow.server.PageConfigurator;
-import com.vaadin.flow.templatemodel.TemplateModel;
 
-@Tag("sa-login-view")
 @Route(value = LoginView.ROUTE)
 @PageTitle("Login")
-public class LoginView extends Component {
+@HtmlImport("frontend://bower_components/iron-form/iron-form.html")
+public class LoginView extends VerticalLayout {
 	public static final String ROUTE = "login";
 
-	public interface Model extends TemplateModel {
-		void setError(boolean error);
+	public LoginView() {
+		TextField userNameTextField = new TextField();
+		userNameTextField.getElement().setAttribute("name", "username");
+		PasswordField passwordField = new PasswordField();
+		passwordField.getElement().setAttribute("name", "password");
+		Button submitButton = new Button("Login");
+		submitButton.setId("submitbutton");
+		UI.getCurrent().getPage().executeJavaScript("document.getElementById('submitbutton').addEventListener('click', () => document.getElementById('ironform').submit());");
+
+		FormLayout formLayout = new FormLayout();
+		formLayout.add(userNameTextField, passwordField, submitButton);
+
+		Element formElement = new Element("form");
+		formElement.setAttribute("method", "post");
+		formElement.setAttribute("action", "login");
+		formElement.appendChild(formLayout.getElement());
+
+		Element ironForm = new Element("iron-form");
+		ironForm.setAttribute("id", "ironform");
+		ironForm.setAttribute("allow-redirect", true);
+		ironForm.appendChild(formElement);
+
+		getElement().appendChild(ironForm);
+
+		setClassName("login-view");
 	}
 }
