16e468b84a7c46cae295d24df0f41feca619a3c1
Paul Römer <paul@vaadin.com>
2019-03-27 14:13:55 +0100

    adds login form component example for form login

--- src/main/java/org/vaadin/paul/spring/ui/views/LoginView.java
+++ src/main/java/org/vaadin/paul/spring/ui/views/LoginView.java
@@ -1,25 +1,21 @@
 package org.vaadin.paul.spring.ui.views;
 
-import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Tag;
-import com.vaadin.flow.component.dependency.HtmlImport;
-import com.vaadin.flow.component.page.Viewport;
-import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
-import com.vaadin.flow.router.AfterNavigationEvent;
-import com.vaadin.flow.router.AfterNavigationObserver;
+import com.vaadin.flow.component.login.LoginForm;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.server.InitialPageSettings;
-import com.vaadin.flow.server.PageConfigurator;
-import com.vaadin.flow.templatemodel.TemplateModel;
 
 @Tag("sa-login-view")
 @Route(value = LoginView.ROUTE)
 @PageTitle("Login")
-public class LoginView extends Component {
+public class LoginView extends VerticalLayout {
 	public static final String ROUTE = "login";
 
-	public interface Model extends TemplateModel {
-		void setError(boolean error);
+	private LoginForm login = new LoginForm();
+
+	public LoginView(){
+		login.setAction("login");
+		getElement().appendChild(login.getElement());
 	}
 }
