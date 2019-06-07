f3c2343e210cf532f11f158aff3d9b2616a96d33
Paul Römer <paul@vaadin.com>
2019-06-07 15:46:38 +0200

    adds proper error handling

--- src/main/java/org/vaadin/paul/spring/ui/views/LoginView.java
+++ src/main/java/org/vaadin/paul/spring/ui/views/LoginView.java
@@ -3,13 +3,17 @@ package org.vaadin.paul.spring.ui.views;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.login.LoginForm;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+
+import java.util.Collections;
 
 @Tag("sa-login-view")
 @Route(value = LoginView.ROUTE)
 @PageTitle("Login")
-public class LoginView extends VerticalLayout {
+public class LoginView extends VerticalLayout implements BeforeEnterObserver {
 	public static final String ROUTE = "login";
 
 	private LoginForm login = new LoginForm();
@@ -17,5 +21,14 @@ public class LoginView extends VerticalLayout {
 	public LoginView(){
 		login.setAction("login");
 		getElement().appendChild(login.getElement());
+	}
+
+	@Override
+	public void beforeEnter(BeforeEnterEvent event) {
+		// inform the user about an authentication error
+		// (yes, the API for resolving query parameters is annoying...)
+		if(!event.getLocation().getQueryParameters().getParameters().getOrDefault("error", Collections.emptyList()).isEmpty()) {
+			login.setError(true);
+		}
 	}
 }
