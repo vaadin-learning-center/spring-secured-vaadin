5157b6864d1b70348521f6c3b88a42ee216a8306
Paul Römer <paul@vaadin.com>
2019-12-06 14:06:24 +0100

    adds server side push

--- src/main/java/org/vaadin/paul/spring/MainView.java
+++ src/main/java/org/vaadin/paul/spring/MainView.java
@@ -1,21 +1,36 @@
 package org.vaadin.paul.spring;
 
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.dom.Element;
 import com.vaadin.flow.dom.ElementFactory;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.annotation.Secured;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Push
 @Route
 @PWA(name = "Project Base for Vaadin Flow with Spring", shortName = "Project Base")
 public class MainView extends VerticalLayout {
     public MainView(@Autowired MessageBean bean) {
-        Button button = new Button("Click me",
-                e -> Notification.show(bean.getMessage()));
+        final Button button = new Button("Click me", e -> {
+            Notification.show(bean.getMessage());
+
+            final UI ui = UI.getCurrent();
+            ExecutorService executor = Executors.newSingleThreadExecutor();
+            executor.submit(() -> {
+                doHeavyStuff();
+                ui.access(() -> {
+                    Notification.show("Calculation done");
+                });
+            });
+        });
         add(button);
 
         // simple link to the logout endpoint provided by Spring Security
@@ -23,4 +38,11 @@ public class MainView extends VerticalLayout {
         getElement().appendChild(logoutLink);
     }
 
+    private void doHeavyStuff() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ex) {
+            // ignore
+        }
+    }
 }
