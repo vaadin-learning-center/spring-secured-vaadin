df13572a053b44880deaea4913a59d128942443f
Marco Zwyssig <marco@zwyssig.info>
2020-05-02 14:58:27 +0000

    updates to V14.0

--- src/main/java/org/vaadin/paul/spring/ui/views/LoginView.java
+++ src/main/java/org/vaadin/paul/spring/ui/views/LoginView.java
@@ -1,5 +1,6 @@
 package org.vaadin.paul.spring.ui.views;
 
+import com.vaadin.flow.component.AttachEvent;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.HtmlImport;
@@ -21,5 +22,10 @@ public class LoginView extends Component {
 
 	public interface Model extends TemplateModel {
 		void setError(boolean error);
+	}
+
+	@Override
+	protected void onAttach(AttachEvent attachEvent) {
+		getElement().setText("Do you wonder why you do not see any login UI? Well, this is the master branch only containing the security setup. For different UI implementations check the other branches :)");
 	}
 }
